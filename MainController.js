b9e4e3f40e2291c7530f94324fb8c58a3d22c44d
orenshor <shororen@post.bgu.ac.il>
2019-06-17 20:11:38 +0300

    restored password

--- MainController.js
+++ MainController.js
@@ -1,35 +1,11 @@
 angular.module("myApp")
-    .controller("MainController", function ($scope) {
-        $(document).ready(function () {
-            function loadXMLDoc() {
-                var xmlhttp = new XMLHttpRequest();
-                xmlhttp.onreadystatechange = function() {
-                    if (this.readyState == 4 && this.status == 200) {
-                        myFunction(this);
-                    }
-                };
-                xmlhttp.open("GET", "./countries.xml", true);
-                xmlhttp.send();
-            }
-            loadXMLDoc();
-            function myFunction(xml) {
-                var x, i, xmlDoc, txt;
-                xmlDoc = xml.responseXML;
-                txt = [];
-                x = xmlDoc.getElementsByTagName("Name");
-                for (i = 0; i < x.length; i++) {
-                    txt.push(x[i].childNodes[0].nodeValue)
-                    //console.log(txt[i]);
-                }
-                // document.getElementById("demo").innerHTML = txt[0];
-                console.log(txt);
-                var x = document.getElementById("countries");
-                for (var i = 0; i < txt.length; i++) {
-                    var option = document.createElement("option");
-                    option.value = txt[i];
-                    option.text = txt[i];
-                    x.appendChild(option);
-                }
-            }
-        });
-    });
+    .controller("MainController", ['$scope', '$http', '$window', function ($scope, $http, $window) {
+        $window.categoryJSON = {
+            1: "Architecture",
+            2: "Museums",
+            3: "Shopping",
+            4: "Restaurants"
+        }
+
+
+    }]);
--- POISController.js
+++ POISController.js
@@ -1,4 +1,57 @@
 angular.module("myApp")
-    .controller("POISController", function ($scope) {
+    .controller("POISController", ['$scope', '$http', '$window', function ($scope, $http, $window) {
+        $scope.clicked = false;
+        const url = `${localUrl}/getRandomPOI`;
+        $http.get(url).then(successRandom, errorRandom);
 
-    });
+
+        function successRandom(response) {
+            $scope.POIs = response.data;
+        }
+
+        function errorRandom(response) {
+            alert("ASDFsdf")
+        }
+
+        $scope.onclick = function () {
+            $scope.clicked = true;
+        }
+
+
+        // clickable divs:
+        $scope.poiDiv = document.getElementById("poi_Div");
+
+        //When the user clicks the button, open the modal
+
+        // $scope.onclick = function(NamePOI) {
+        //     // $scope.divText = '<span class="close">&times;</span><h2>'+$scope.POIs[0].NamePOI+'</h2><br><br><p id="content">Category: {{poi.CategoryID}}<br>{{poi.Location}}<br>{{poi.Details}}<br>Rank: {{poi.Rank}}<br>Number Of Viewers: {{poi.numOfViews}}br></p>';
+        //     // document.getElementById("poiName").innerHTML = $scope.POIs[NamePOI].NamePOI;
+        //     $scope.divToShow = null;
+        //     $scope.divToShow = document.getElementById("divTOshow");
+        //     $scope.divToShow.style.display = "block";
+        // }
+        var span = document.getElementsByClassName("close");
+        // When the user clicks on <span> (x), close the modal
+
+        span.onclick = function() {
+            $scope.divToShow = document.getElementById("divTOshow");
+            $scope.divToShow.style.display = "none";
+        }
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+            $scope.divToShow = document.getElementById("divTOshow");
+            if (event.target == $scope.divToShow) {
+
+                $scope.divToShow.style.display = "none";
+            }
+        }
+        window.addEventListener("keydown", function(event){
+            if(event.code == "Escape"){
+                $scope.divToShow = document.getElementById("divTOshow");
+                $scope.divToShow.style.display = "none";
+            }
+        });
+
+        // $scope.POIs = response.data;
+    }]);
